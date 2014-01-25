2ed2856a4e28522017951605b71d180923f92af9
Piero Ranalli <piero@MacBook-Air-di-Piero-2.local>
2014-01-25 23:10:59 +0200

    (only whitespace changed)

--- lib/PSF.pm
+++ lib/PSF.pm
@@ -1,18 +1,18 @@
-# the XMM-CDFS simulator of astronomical X-ray observations                                                                      
-# Copyright (C) 2013 Piero Ranalli                                                                                               
-#                                                                                                                                
-#  This program is free software: you can redistribute it and/or modify                                                          
-#  it under the terms of the GNU Affero General Public License as                                                                
-#  published by the Free Software Foundation, either version 3 of the                                                            
-#  License, or (at your option) any later version.                                                                               
-#                                                                                                                                
-#  This program is distributed in the hope that it will be useful,                                                               
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                                 
-#  GNU Affero General Public License for more details.                                                                           
-#                                                                                                                                
-#  You should have received a copy of the GNU Affero General Public License                                                      
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.                                                         
+# the XMM-CDFS simulator of astronomical X-ray observations
+# Copyright (C) 2013 Piero Ranalli
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
@@ -65,7 +65,7 @@ sub new {
     # locate PSF library
     my $CCF;
     #printf("camera=%s\n",$camera);
-    if ($oldpsf or not defined($camera)) { 
+    if ($oldpsf or not defined($camera)) {
 	$CCF = $ENV{SAS_CCFPATH}.'/XRT1_XPSF_0007.CCF';
     } else {
 	if ($camera eq 'EMOS1') {
